a013e5da817e5d273b5ed45b0f6f47f5bd7ae4f8
Divaker Singh <122163074+divaker75@users.noreply.github.com>
2023-08-22 17:56:21 +0530

    Update bubble_sort.js

--- scripts/bubble_sort.js
+++ scripts/bubble_sort.js
@@ -1,6 +1,6 @@
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:-   DIVAKER SINGH
     
     *****************
 */
@@ -46,7 +46,7 @@ function Bubble()
 
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:-   DIVAKER SINGH
     
     *****************
 */
